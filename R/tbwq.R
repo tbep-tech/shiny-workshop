36afaf3c4eab39c66e821727c463c14c4933e557
fawda123 <mbafs2012@gmail.com>
2023-05-22 08:20:20 -0400

    save tbwq r file

--- R/tbwq.R
+++ R/tbwq.R
@@ -4,6 +4,10 @@ library(here)
 
 tbwqdat <- epcdata %>%
   filter(yr == 2021)  %>%
-  select(lat = Latitude, lon = Longitude, bay_segment, station = epchc_station, mo, chla)
+  select(lat = Latitude, lon = Longitude, bay_segment, station = epchc_station, mo, chla) %>%
+  mutate(
+    lat = round(lat, 3),
+    lon = round(lon, 3)
+  )
 
 write.csv(tbwqdat, file = here('data/tbwqdat.csv'), row.names = F)
